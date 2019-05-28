1b6e9669fd928e62372f73a34b475c308852a1e7
github username <dekadekadeka@gmail.com>
2019-05-28 10:05:56 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,16 @@
 class SessionsController < ApplicationController
     def new
-        @user = User.new
     end
 
     def create
-        @user = User.find_by(name: params[:user][:name])
-        return head(:forbidden) unless @user.authenticate(params[:user][:password])
+        @user = User.find_by(name: session_params[:name])
+        return head(:forbidden) unless @user.authenticate(session_params[:password])
         session[:user_id] = @user.id
+    end
+
+    private
+
+    def session_params
+        params[:user]
     end
 end
